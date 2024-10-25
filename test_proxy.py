384c78f43951c031ba0809634f15c0fbc97539ca
minyakonga <minyakonga@gmail.com>
2024-10-25 08:42:30 +0800

    add more test case

--- test_proxy.py
+++ test_proxy.py
@@ -25,3 +25,13 @@ async def test_httpsbin():
         async with session.get(url) as resp:
             assert resp.status == 200
             assert await resp.json() is not False
+
+@pytest.mark.asyncio
+async def test_httpsgist():
+    url = 'https://gist.githubusercontent.com/csrgxtu/9c3d4303e262bf5333cc57ff11ea9105/raw/d57dcf1b58d76b5e543618e203f8bffb3fa141d9/gistfile1.txt'
+    connector = ProxyConnector.from_url(PROXY_ADDR)
+
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+            print(await resp.text())
