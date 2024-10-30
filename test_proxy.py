caaf1ef647c36ddf9f7443e02e7ead1216d21407
qcloud <ubuntu@localhost.localdomain>
2024-10-30 21:45:32 +0800

    seems tencent vps banned google.com packets

--- test_proxy.py
+++ test_proxy.py
@@ -3,8 +3,8 @@ import aiohttp
 from aiohttp_socks import ProxyType, ProxyConnector, ChainProxyConnector
 
 
-PROXY_ADDR = 'socks5://username:password@localhost:3000'
-# PROXY_ADDR = 'socks5://username:password@43.153.3.156:3000'
+# PROXY_ADDR = 'socks5://username:password@localhost:3000'
+PROXY_ADDR = 'socks5://username:password@43.153.3.156:3000'
 
 
 @pytest.mark.asyncio
@@ -51,6 +51,26 @@ async def test_httpsgoogle():
 @pytest.mark.asyncio
 async def test_httpsbaidu():
     url = 'https://baidu.com'
+    connector = ProxyConnector.from_url(PROXY_ADDR)
+
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url, allow_redirects=False) as resp:
+            assert resp.status == 302
+            print(await resp.text())
+
+@pytest.mark.asyncio
+async def test_httpsyoutube():
+    url = 'https://youtube.com'
+    connector = ProxyConnector.from_url(PROXY_ADDR)
+
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url, allow_redirects=False) as resp:
+            assert resp.status == 301
+            print(await resp.text())
+
+@pytest.mark.asyncio
+async def test_httpstwitter():
+    url = 'https://twitter.com'
     connector = ProxyConnector.from_url(PROXY_ADDR)
 
     async with aiohttp.ClientSession(connector=connector) as session:
